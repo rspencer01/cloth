9fc1a8a852bc6fa9b5e9911aa1185f6a27652ca9
Robert Spencer <robert.spencer94@gmail.com>
2013-08-20 08:21:45 +0200

    Added basic 2d vector

---
+++ simul/vector2d.cpp
@@ -0,0 +1,29 @@
+#include "vector2d.h"
+vector2D::vector2D()
+{
+  x = y = 0.0;
+}
+
+vector2D::vector2D(float _x,float _y)
+{
+  x = _x; y = _y;
+}
+vector2D& vector2D::operator=(const vector2D &rhs) 
+{
+  x = rhs.x; y = rhs.y;
+  return *this;
+}
+const vector2D vector2D::operator+(const vector2D &other) const 
+{
+  vector2D result = *this;
+  result.x = x + other.x;
+  result.y = y + other.y;
+  return result;
+}
+const vector2D vector2D::operator*(const float &other) const 
+{
+  vector2D result = *this; 
+  result.x *= other;     
+  result.y *= other;
+  return result;
+}
---
+++ simul/vector2d.h
@@ -0,0 +1,11 @@
+class vector2D
+{
+  public:
+    float x;
+    float y;
+    vector2D();
+    vector2D(float _x,float _y);
+    vector2D& operator=(const vector2D &rhs);
+    const vector2D operator+(const vector2D &other) const ;
+    const vector2D operator*(const float &other) const ;
+};
