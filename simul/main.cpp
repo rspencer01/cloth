f6a29fcc0e82e7fd2ae52fa1c2f9bc8561b2a78d
Robert Spencer <robert.spencer94@gmail.com>
2013-08-20 10:04:19 +0200

    Making vectors and basic sheet simulation

--- simul/main.cpp
+++ simul/main.cpp
@@ -6,19 +6,25 @@
 #include <time.h>
 #include <utility>
 #include <vector>
+#include "vector2d.h"
 using namespace std;
 
-#define FRAME_COUNT 100
-#define WIDTH 10
-#define HEIGHT 15
+#define FRAME_COUNT 1000
+#define WIDTH 52
+#define HEIGHT 65
+#define POINT_MASS 1 
+#define TIMESTEP 1
+#define K 0.2
+#define FRICTIONAL_CONSTANT 0.01
 
-typedef pair<float,float> pff;
-typedef vector<vector<pff> > frame;
+typedef vector<vector<vector2D> > frame;
 
 FILE* fp;
 frame currFrame;
 frame nextFrame;
-frame momenta;
+frame velocities;
+frame accelerations;
+float constraintLength;
 
 void openFile()
 {
@@ -39,23 +45,25 @@ void writeHeader()
 
 void initialiseFrame()
 {
+  constraintLength = 0.5/HEIGHT;
   currFrame.clear();
-  momenta.clear();
+  velocities.clear();
   nextFrame.clear();
   for (int i = 0;i<HEIGHT;i++)
   {
-    vector<pff> tem;
-    vector<pff> tem2;
+    vector<vector2D> tem;
+    vector<vector2D> tem2;
     for (int j=0;j<WIDTH;j++)
     {
-      pff t = make_pair(i/float(HEIGHT)-0.5,-j/float(WIDTH));
-      tem.push_back(t);
-      tem2.push_back(make_pair(0,-0.001));
+      tem.push_back(vector2D(i/float(HEIGHT)*1.6-0.5*1.6,0.9-j/float(HEIGHT*5)));
+      tem2.push_back(vector2D(0,0));
     }
     currFrame.push_back(tem);
     nextFrame.push_back(tem);
-    momenta.push_back(tem2);
+    velocities.push_back(tem2);
+    accelerations.push_back(tem2);
   }
+  currFrame[0][HEIGHT-1] = currFrame[0][HEIGHT-1] - vector2D(-0.1,0.1);
 }
 
 void writeFrame(frame x)
@@ -63,7 +71,7 @@ void writeFrame(frame x)
   for (int j = 0;j<HEIGHT;j++)
     for (int i = 0;i<WIDTH;i++)
     {
-      fprintf(fp,"%f %f ",x[j][i].first,x[j][i].second);
+      fprintf(fp,"%f %f ",x[j][i].x,x[j][i].y);
     }
   fprintf(fp,"\n");
 }
@@ -75,16 +83,55 @@ void copyBackFrame()
       currFrame[j][i]= nextFrame[j][i];
 }
 
-pff add(pff a,pff b)
-{
-  return make_pair(a.first+b.first,a.second+b.second);
-}
-
 void updatePosition()
 {
   for (int j = 0;j<HEIGHT;j++)
     for (int i = 1;i<WIDTH;i++)
-      nextFrame[j][i] = add(currFrame[j][i],momenta[j][i]);
+      nextFrame[j][i] = currFrame[j][i]+velocities[j][i]*TIMESTEP;
+}
+
+void updateAccelerations()
+{
+  // Gravity
+  for (int j = 0;j<HEIGHT;j++)
+    for (int i = 1;i<WIDTH;i++)
+      accelerations[j][i] = vector2D(0,-0.0001);
+
+  for (int j = 0;j<HEIGHT;j++)
+    for (int i = 1;i<WIDTH;i++)
+    {
+      vector2D diff = currFrame[j][i]-currFrame[j][i-1];
+      if (diff.mag()>constraintLength)
+      {
+        diff = diff - diff*(constraintLength/diff.mag());
+        accelerations[j][i] = accelerations[j][i] - diff * K;
+        if (i>0)
+          accelerations[j][i-1] = accelerations[j][i-1] + diff * K;
+      }
+    }
+  for (int j = 1;j<HEIGHT;j++)
+    for (int i = 0;i<WIDTH;i++)
+    {
+      vector2D diff = currFrame[j][i]-currFrame[j-1][i];
+      if (diff.mag()>constraintLength)
+      {
+        diff = diff - diff*(constraintLength/diff.mag());
+        accelerations[j][i] = accelerations[j][i] - diff * K;
+        accelerations[j-1][i] = accelerations[j-1][i] + diff * K;
+      }
+    }
+  
+  for (int j = 0;j<HEIGHT;j++)
+    for (int i = 1;i<WIDTH;i++)
+      accelerations[j][i] = accelerations[j][i] - velocities[j][i]*FRICTIONAL_CONSTANT;
+
+}
+
+void updateVelocities()
+{
+  for (int j = 0;j<HEIGHT;j++)
+    for (int i = 1;i<WIDTH;i++)
+      velocities[j][i] = velocities[j][i] + accelerations[j][i]*TIMESTEP;
 }
 
 int main(int argc, char**argv)
@@ -95,6 +142,8 @@ int main(int argc, char**argv)
   for (int i = 0;i<FRAME_COUNT;i++)
   {
     writeFrame(currFrame);
+    updateAccelerations();
+    updateVelocities();
     updatePosition();
     copyBackFrame();
   }
